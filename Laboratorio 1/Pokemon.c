8585e84f07ef38f75ee30cd1045932d4037a4962
Ignacio Trujillo <83096192+KilluvDev@users.noreply.github.com>
2024-03-25 16:16:45 -0300

    Update Pokemon.c

--- Laboratorio 1/Pokemon.c
+++ Laboratorio 1/Pokemon.c
@@ -2,18 +2,39 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h> 
 
-#define BUFFER_SIZE 4096
+void crearDirectoriosAnidados(char *ruta, int profundidad) {
+    if (profundidad == 0) return;
+    for (char letra = 'A'; letra <= 'Z'; letra++) {
+        char nuevoDirectorio[256];
+        sprintf(nuevoDirectorio, "%s/%c", ruta, letra);
+        if (mkdir(nuevoDirectorio, 0777) == 0) {
+            crearDirectoriosAnidados(nuevoDirectorio, profundidad - 1);
+        }
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
 
-	DIR *folder, *check, *subfolder;
-	FILE *archivo, *archivo_origen, *archivo_destino;
-	struct dirent *directorio, *subdirectorio;
-	char buffer[BUFFER_SIZE], alfaname, genname;
-	int counter;
-	size_t bytes_leidos;
+	DIR *folder, *check;
+	FILE *archivoOrigen, *archivoDestino;
+	char buffer[1024];
+    size_t bytesLeidos;
+	struct dirent *directorio;
+
+	mkdir("./Generación",0777);
+	mkdir("./Generación/I",0777);
+	mkdir("./Generación/II",0777);
+	mkdir("./Generación/III",0777);
+	mkdir("./Generación/IV",0777);
+	mkdir("./Alfabético",0777);
+
+	char rutaInicial[] = "./Alfabético/"; // Comienza en el directorio actual
+    int profundidad = 1; // Profundidad de anidación
+    crearDirectoriosAnidados(rutaInicial, profundidad);
 
 	folder = opendir("./Sprites"); // Directorio a registrar
 	if (folder == NULL)
@@ -22,50 +43,19 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	// PAUSAO
-	while ((directorio = readdir(folder)))
-	{
-		// printf("%c\n", directorio->d_name[0]);
-		alfaname = directorio->d_name[0];
-		archivo_origen = fopen(directorio->d_name, "rb");
-		check = opendir(alfaname);
-		if (check == NULL)
-		{
-			mkdir(alfaname, 0777);
-			archivo_destino = fopen(directorio, "wb");
-		}
-		else
-		{
-			archivo_destino = fopen(directorio, "wb");
-		}
-		while ((bytes_leidos = fread(buffer, 1, BUFFER_SIZE, archivo_origen)) > 0)
-		{
-			fwrite(buffer, 1, bytes_leidos, archivo_destino);
-		}
-		fclose(archivo_origen);
-		fclose(archivo_destino);
+	while((directorio = readdir(folder))){
+		printf("%s\n", directorio->d_name);
 	}
+	closedir(folder);
 
-	/*
-	folder = opendir("./Generación");
-	while ((directorio = readdir(folder)))
-	{
-	}
-	*/
 
-	// CONTADOR POR LETRA
-	archivo = fopen("RegistroPokemon.txt", "w");
-	folder = opendir("./Alfabetico");
-	while ((directorio = readdir(folder)))
-	{
-		counter = 0;
-		subfolder = opendir(directorio->d_name);
-		while ((subdirectorio = readdir(subfolder)))
-		{
-			counter++;
-		}
-		fputs(("%c - %d\n", directorio->d_name, counter), "RegistroPokemon.txt");
-	}
-	fclose(archivo);
+
 	return 0;
 }
+
+
+
+
+
+// gcc -o out Pokemon.c
+// ./out
