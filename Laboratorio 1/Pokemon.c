508c2374c66c60320d8a28b080d88d5f9d2c62b2
Ignacio Trujillo <83096192+KilluvDev@users.noreply.github.com>
2024-03-25 12:05:52 -0300

    Update Pokemon.c

--- Laboratorio 1/Pokemon.c
+++ Laboratorio 1/Pokemon.c
@@ -2,49 +2,43 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
-#define BUFFER_SIZE 4096
+void Organizacion(){
+	mkdir("./Generación",0777);
+	mkdir("./Generación/I",0777);
+	mkdir("./Generación/II",0777);
+	mkdir("./Generación/III",0777);
+	mkdir("./Generación/IV",0777);
+	
+	char dirName[2]="A";
+	for(int i=0;i<26;i++){
+		mkdir(dirName)
+	}
+
+}
+
 
 int main(int argc, char *argv[])
 {
 
-	DIR *folder, *check, *subfolder;
-	FILE *archivo, *archivo_origen, *archivo_destino;
-	struct dirent *directorio, *subdirectorio;
-	char buffer[BUFFER_SIZE], alfaname, genname;
-	int counter;
-	size_t bytes_leidos;
-
+	DIR *folder, *check;
+	FILE *archivo,*copia;
+	struct dirent *directorio;
+	Organizacion();
 	folder = opendir("./Sprites"); // Directorio a registrar
 	if (folder == NULL)
 	{
 		printf("Error al abrir la carpeta");
 		return 1;
 	}
+	return 0;
+}
 
-	// PAUSAO
-	while ((directorio = readdir(folder)))
-	{
-		// printf("%c\n", directorio->d_name[0]);
-		alfaname = directorio->d_name[0];
-		archivo_origen = fopen(directorio->d_name, "rb");
-		check = opendir(alfaname);
-		if (check == NULL)
-		{
-			mkdir(alfaname, 0777);
-			archivo_destino = fopen(directorio, "wb");
-		}
-		else
-		{
-			archivo_destino = fopen(directorio, "wb");
-		}
-		while ((bytes_leidos = fread(buffer, 1, BUFFER_SIZE, archivo_origen)) > 0)
-		{
-			fwrite(buffer, 1, bytes_leidos, archivo_destino);
-		}
-		fclose(archivo_origen);
-		fclose(archivo_destino);
-	}
+
+
+
+
 
 	/*
 	folder = opendir("./Generación");
@@ -54,6 +48,7 @@ int main(int argc, char *argv[])
 	*/
 
 	// CONTADOR POR LETRA
+    /*
 	archivo = fopen("RegistroPokemon.txt", "w");
 	folder = opendir("./Alfabetico");
 	while ((directorio = readdir(folder)))
@@ -67,5 +62,9 @@ int main(int argc, char *argv[])
 		fputs(("%c - %d\n", directorio->d_name, counter), "RegistroPokemon.txt");
 	}
 	fclose(archivo);
-	return 0;
-}
+    */
+
+
+
+// gcc -o out Pokemon.c
+// ./out
