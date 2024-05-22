ca60f32962ae81f720d4df468582ea0d96e4dd92
Ignacio Trujillo <83096192+KilluvDev@users.noreply.github.com>
2024-05-22 01:34:11 -0400

    Update main.java

--- Laboratorio 3/main.java
+++ Laboratorio 3/main.java
@@ -9,11 +9,12 @@ final class main {
         pos = map.posicion("ArchivosDePrueba/15x15/laberinto.txt");
         String[][] mapa;
         mapa = map.Map("ArchivosDePrueba/15x15/laberinto.txt");
-        System.out.println(n);
-        System.out.println(pos[0] + " " + pos[1]);
 
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
+        // mov.MovFork(mapa, pos);
+        // mov.MovThread(mapa, pos);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(mapa[i][j]);
             }
             System.out.println();
