c57fe1fd20efc00faa8f510ae9a072a31f6a9d6b
Ignacio Trujillo <83096192+KilluvDev@users.noreply.github.com>
2024-05-22 00:48:30 -0400

    Update main.java
    Call of methods, gets size 'n' and map.

--- Laboratorio 3/main.java
+++ Laboratorio 3/main.java
@@ -1,13 +1,19 @@
 final class main {
 
     public static void main(String[] args) {
-        // Leer un txt
         archivo map = new archivo();
+        movimiento mov = new movimiento();
+        int n;
+        int[] pos;
+        n = map.size("ArchivosDePrueba/15x15/laberinto.txt");
+        pos = map.posicion("ArchivosDePrueba/15x15/laberinto.txt");
         String[][] mapa;
         mapa = map.Map("ArchivosDePrueba/15x15/laberinto.txt");
+        System.out.println(n);
+        System.out.println(pos[0] + " " + pos[1]);
 
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
                 System.out.print(mapa[i][j]);
             }
             System.out.println();
